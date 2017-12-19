9e991f5f6d91bf75198bfc1f371422227d476788
Sean Oughton <seanoughton@gmail.com>
2017-12-19 23:21:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,6 +6,6 @@ function addElementToBeginningOfArray(array,element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element) {
- array.unshift(elment)
+ array.unshift(element)
  return array 
 }
