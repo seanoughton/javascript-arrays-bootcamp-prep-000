30f0726b1904b90e306ed06ad338a49b984c5ef8
Sean Oughton <seanoughton@gmail.com>
2017-12-19 23:24:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,7 @@ function destructivelyAddElementToBeginningOfArray(array,element) {
 }
 
 function addElementToEndOfArray(array,element){
-  newArray = [array,... element]
+  newArray = [...array,element]
   return newArray
 }
 
