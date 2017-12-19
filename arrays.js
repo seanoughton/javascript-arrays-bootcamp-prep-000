982f9f9729ea4844efee63be04a405036471fe61
Sean Oughton <seanoughton@gmail.com>
2017-12-19 23:17:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,8 +2,10 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array,element){
   
+  return array
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element) {
-  
+ 
+ return array 
 }
